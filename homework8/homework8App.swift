3add7e60f2e5346c021fd64cad673cd183bb30cf
Kelvin <kelvinchao@protonmail.com>
2025-10-07 14:59:10 -0700

    Initial Commit

---
+++ homework8/homework8App.swift
@@ -0,0 +1,17 @@
+//
+//  homework8App.swift
+//  homework8
+//
+//  Created by Kelvin Chao on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct homework8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
